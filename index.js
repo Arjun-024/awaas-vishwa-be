21399deedb207daa650b691ac8692c2c3d616e00
Arjun024 <mallikarjuna.gunisetti@gmail.com>
2023-08-01 17:34:59 +0530

    feat: user register

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const healthRoute = require('./routes/HealthRoute')
+const authRoutes = require('./routes/AuthRoutes')
 
 /**
  * APP
  */
 const app = express();
 app.use(express.json())
-
+app.use(cors())
 
 /**
  * DATABASE CONNCECTION
@@ -18,7 +20,7 @@ mongoose.connection.once('connected', () => {console.log('Databse connected')})
 mongoose.connection.on('error', (er) => {console.log('Database error', er)})
 
 app.use('/health', healthRoute)
-
+app.use('/api/v1/auth', authRoutes)
 
 /**
  * SERVER LISTEM
